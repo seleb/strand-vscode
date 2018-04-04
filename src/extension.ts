0553f93dfc2cf142a7c7d8a8a3c7ae67041cfde4
Sean LeBlanc <sean.s.leblanc@gmail.com>
2018-04-04 09:28:56 -0400

    added symbol provider

---
+++ src/extension.ts
@@ -0,0 +1,38 @@
+"use strict";
+import * as vscode from "vscode";
+
+export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(
+		vscode.languages.registerDocumentSymbolProvider(
+			{ language: "strand" },
+			new StrandDocumentSymbolProvider()
+		)
+	);
+}
+
+class StrandDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
+	public provideDocumentSymbols(
+		document: vscode.TextDocument,
+		token: vscode.CancellationToken
+	): Thenable<vscode.SymbolInformation[]> {
+		return new Promise((resolve, reject) => {
+			const symbols: vscode.SymbolInformation[] = [];
+
+			for (let i = 0; i < document.lineCount; i++) {
+				const line = document.lineAt(i);
+				if (line.text.startsWith("::")) {
+					symbols.push(
+						new vscode.SymbolInformation(
+							line.text.substr(2),
+							vscode.SymbolKind.Object,
+							"passage",
+							new vscode.Location(document.uri, line.range)
+						)
+					);
+				}
+			}
+
+			resolve(symbols);
+		});
+	}
+}
