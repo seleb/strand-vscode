ca49d92ad5a7979dcc2f777c5226cd58c217e866
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2024-04-24 15:45:52 -0400

    add code folding

--- src/extension.ts
+++ src/extension.ts
@@ -4,21 +4,26 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
 		vscode.languages.registerDocumentSymbolProvider({ language: 'strand' }, new StrandDocumentSymbolProvider()),
+		vscode.languages.registerFoldingRangeProvider({ language: 'strand' }, new StrandFoldingRangeProvider())
 	);
+}
+
+function getPassageLines(document: vscode.TextDocument) {
+	const passageLines: number[] = [];
+	for (let i = 0; i < document.lineCount; ++i) {
+		const line = document.lineAt(i);
+		if (line.text.startsWith('::')) {
+			passageLines.push(i);
+		}
+	}
+	return passageLines;
 }
 
 class StrandDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
 	public provideDocumentSymbols(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.ProviderResult<vscode.SymbolInformation[]> {
 		const symbols: vscode.SymbolInformation[] = [];
 
-		const passageLines: number[] = [];
-
-		for (let i = 0; i < document.lineCount; ++i) {
-			const line = document.lineAt(i);
-			if (line.text.startsWith('::')) {
-				passageLines.push(i);
-			}
-		}
+		const passageLines = getPassageLines(document);
 
 		for (let i = 0; i < passageLines.length - 1; ++i) {
 			const line = document.lineAt(passageLines[i]);
@@ -35,5 +40,16 @@ class StrandDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
 		return symbols;
 	}
 }
+
+class StrandFoldingRangeProvider implements vscode.FoldingRangeProvider {
+	public provideFoldingRanges(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.ProviderResult<vscode.FoldingRange[]> {
+		const foldingRanges: vscode.FoldingRange[] = [];
+
+		const passageLines = getPassageLines(document);
+
+		for (let i = 0; i < passageLines.length - 1; ++i) {
+			foldingRanges.push(new vscode.FoldingRange(passageLines[i], passageLines[i + 1] - 1));
+		}
+		return foldingRanges;
 	}
 }
