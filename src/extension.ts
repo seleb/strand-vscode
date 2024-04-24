b0a072b1549e8ea958f09afbb335fb1ff2de1580
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2024-04-24 15:42:27 -0400

    passage symbols cover their whole range instead of just the header line

--- src/extension.ts
+++ src/extension.ts
@@ -11,11 +11,25 @@ class StrandDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
 	public provideDocumentSymbols(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.ProviderResult<vscode.SymbolInformation[]> {
 		const symbols: vscode.SymbolInformation[] = [];
 
-		for (let i = 0; i < document.lineCount; i++) {
+		const passageLines: number[] = [];
+
+		for (let i = 0; i < document.lineCount; ++i) {
 			const line = document.lineAt(i);
 			if (line.text.startsWith('::')) {
-				symbols.push(new vscode.SymbolInformation(line.text.substring(2), vscode.SymbolKind.Object, 'passage', new vscode.Location(document.uri, line.range)));
+				passageLines.push(i);
 			}
+		}
+
+		for (let i = 0; i < passageLines.length - 1; ++i) {
+			const line = document.lineAt(passageLines[i]);
+			symbols.push(
+				new vscode.SymbolInformation(
+					line.text.substring(2),
+					vscode.SymbolKind.Object,
+					'passage',
+					new vscode.Location(document.uri, new vscode.Range(new vscode.Position(passageLines[i], 0), new vscode.Position(passageLines[i + 1] - 1, 0)))
+				)
+			);
 		}
 
 		return symbols;
